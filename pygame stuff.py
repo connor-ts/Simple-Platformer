ee8cc84a6b4c701cd778ae39f8f2b2e82d1c8e47
connor <connorterstege2@gmail.com>
2026-01-15 18:04:48 -0800

    rewrote a lot the code to make it simpler, finally have a working game to an extent
    Refactor and update platformer game code with new features and improved structure.

--- pygame stuff.py
+++ pygame stuff.py
@@ -1,162 +1,167 @@
 # connor ter stege
-# uhhh today is the 13th of jan
-# platformer game in pygame
-# ai might be used to save time on repeat tasks or unknown errors, if i didnt use it ill remove this.. nuless i forget :p
-## i barely comment on my personal code so this is probably gonna be littered with comments of what things do, ill try to avoid obvious ones
-## my common variable uses are the first letters and last letters of the alphabet unless its specifically named -> a,b,c,x,y,z
-## ill provide links to sources if i use them for reference or if i think they might help in understanding where i gained and used information i may of needed a refresher on or we didnt discuss in class
-### might not use math
-### if u see :something: in a comment i am used to doing that on discord bc thats how u type out emojis there 
+# 13th of jan
+# scuffed platformer game in pygame
 
-import pygame, random, math, os # used os possibly because coding on both mac and windows and certain commands can help make sure code still works on both versions :)
-import sys # helps close the active window faster and easier on windows, probably will remove after final push
+## Uses of ai ->
+## I personally wouldnt say im fluent in python as its not my main language so some 
+## LLM and google searches will be used to find out how to solve errors as my main goal 
+## is just to make a working game that i like rather than doing something thats not fully what i wanted but 100% me
+
+# things i plan to add, sprite thing with a cat image
+# maybe double jump
+# dashing maybe, because thats quite hard to do
+
+import pygame
+import random
+import sys # i might remove this later because its only really helpful in my eyes to windows 11
+
 pygame.init()
 
-### screen /// setup
-s_x, s_y = 1280, 720 # storing into a variable to make it easier to apply in later loops and code
-screen = pygame.display.set_mode((s_x, s_y)) # 720p reso
-pygame.display.set_caption("low quality platformer")
+# setup stuffs
+screen = pygame.display.set_mode((1280, 720)) # i dont remember the actual res of the screens at 
+# school but i dont want to make them too large or small so i assume that 720p reso- is enough :p
+pygame.display.set_caption("simple platformer") # i might think of a name later, but honestly this good lwk
 clock = pygame.time.Clock()
-# im also kinda js banking on the fact that mac and windows have the same listing of fonts so its not a different font when ur on mac :sob:
-font = pygame.font.SysFont(None, 36) # lets me search system fonts and use system fonts
 
-# platforms
-platforms = [
-    pygame.Rect(300, 500, 200, 20),
-    pygame.Rect(600, 400, 150, 20),     
-    pygame.Rect(900, 300, 200, 20)         
-]
+# fonts if i add more later maybe
+font1 = pygame.font.SysFont(None, 48) # lots of info from here especially for sys stuff: https://www.pygame.org/docs/ref/font.html
 
-# random variables 
+# game state variable
+state = "menu"
 
-groundY = 600
-pgapx = (220, 350)
-pgapy = (-120, 120)
+# start button drawing
+start = pygame.Rect(1280 // 2 - 100, 720 // 2 - 40, 200, 80) 
 
-# play stuffs lol
-class Player:
+# color palette
+Background = (130, 184, 189) # just used google to search up rgb for a light blue
+White = (255, 255, 255) ## same here
+Green = (55, 163, 84) ## same here
+
+# cube/player
+class cube:
     def __init__(self, x, y):
-        ##size and pos
-        self.rect = (x, y, 50, 50) 
-        ##movement
-        self.speed = 5
-        self.velocity_xaxis = 0      # velocity x
-        self.velocity_yaxis = 0      # velocity y
-        self.jpower = -15       # how strong the jump is
-        self.grav = 1    # normal gravity
-        ###
-        self.jpressed = False
-        self.jcount = 0
-        self.grounded = False  # checks if the player is standing on something, may help later with adding in features once the boilerplate is done
-        ##dash
-        self.dashing = False
-        self.dash_s = 20 #dash speed
-        self.dash_t = 10 #dash time
-        self.dash_timer = 0 # dash timer /// not to be confused with time, time of dash is how long the physical dash is versus the timer which is in between 
-        self.dash_c = 180 # time between dashes
-        self.dash_cd_timer = 0 
-        self.dash_p = False # checks if the Lshift is pressed /// held counts and will not allow dashing if Lshift is pressed again
+        self.rect = pygame.Rect(x, y, 40, 40)
+        self.y_velocity = 0 
+        self.speed = 7 # speed of the cube
+        self.jumpstrength = -15 # might make this lower or higher depending on how easy it is
+        self.onground = False # ground variable checks if it was
+    def movement(self, platforms, keyboard):
+        if keyboard[pygame.K_a]:
+            self.rect.x -= self.speed
+            for x in platforms:
+                if self.rect.colliderect(x): # the info i found on colliderect was found mostly this pygame doc: https://www.pygame.org/docs/ref/rect.html
+                    self.rect.left = x.right
+        if keyboard[pygame.K_d]:
+            self.rect.x += self.speed
+            for x in platforms:
+                if self.rect.colliderect(x): ## same here https://www.pygame.org/docs/ref/rect.html
+                    self.rect.right = x.left  
+        if keyboard[pygame.K_SPACE] and self.onground: # controls jumping using space bar
+            self.y_velocity = self.jumpstrength
+            self.onground = False   # stops inf jumping when ur in the sky
+        self.y_velocity += 1 # gravity logic so that the cube comes down after u press space, this was a pain
+        if self.y_velocity > 20: # terminal velocity cap, because somehow it was causing issues with the collision 
+            self.y_velocity = 20 ## not a lot but some helpful advice was found: https://www.pygame.org/docs/ref/math.html // but mostly on google
+        self.rect.y += self.y_velocity
 
-    def movement(self, keys, platforms):
-        # normal movement stuff
-        move = 0
-        if keys[pygame.K_a]: # key -> a
-            move = -1
-        if keys[pygame.K_d]: # key -> d
-            move = 1
-        if not self.dashing: # i think this is right, i mean it shouldnt break 
-            pass # it should mean that when u dash when not moving itll like not move you 
-## jumping func     
-        if keys[pygame.K_SPACE] and not self.jpressed:
-            if self.jcount < 2: # max of 2 jumps might change this later if i wanna make like fancy stuff with the jumping or like upgrades
-                self.velocity_yaxis = self.jpower
-                self.jcount += 1 # just adds one jump to the counter to keep track so that u dont jump more than 2 jumps
-            self.jpressed = True
-        if not keys[pygame.K_LSHIFT]: # basically if the key aint pressed dont jump ;p
-            self.jpressed = False
+### LLM was used to fix
+        # Collision detection
+        self.onground = False
+        for p in platforms:
+            if self.rect.colliderect(p):
+                if self.y_velocity >= 0:  # falling down
+                    self.rect.bottom = p.top
+                    self.y_velocity = 0
+                    self.onground = True
+                elif self.y_velocity < 0:  # moving up (hit head)
+                    self.rect.top = p.bottom
+                    self.y_velocity = 0
+### end
+## explaination:
+# i was having lots of issues keeping the platforms collidable and non collidable, 
+# all this code basically supports is it arranges the logic of the cube and platforms to where when the cube
+# touches another rectangle as the background isnt a rectangle it can stand, touch, etc on top of the rectangle
+## ive intentionally left the ai comments so u can see the difference between the code.
 
-        if keys[pygame.K_LSHIFT] and not self.dash_p and self.dash_cd_timer <= 0: # checks that the timer is 0 so u cant dash spam
-            self.dashing = True 
-            self.dash_timer = self.dash_t
-            self.dash_cd_timer = self.dash_c
-            self.velocity_yaxis = self.dash_speed if move >= 0 else -self.dash_speed # ai to figure this out because i thinky it looks weird that theres an if and else on the same line
-            self.dash_p = True
-        if not keys[pygame.K_LSHIFT]: # keeps lshift from being held down to dash multiple times
-            self.dash_p = False
-        if self.dashing: # happens if the player is dashing
-            self.rect.x += self.velocity_xaxis
-            self.dash_timer -= 1
-            if self.dash_timer <= 0: # if the timer hits 0 it basically js stops the increased movement 
-                self.dashing = False # ends the dash
-                self.velocity_xaxis = 0
-# timer stuffs
-        if self.dash_cd_timer > 0:
-            self.dash_cd_timer -= 1
-# gravity 
-        self.velocity_yaxis += self.grav
-        self.rect.y += self.velocity_yaxis
-# collision
-        self.grounded = False # understands that the player class is not grounded until it checks for collision with the platforms
-        for b in platforms:
-## for info on colliderect: https://www.pygame.org/docs/ref/rect.html#pygame.Rect.colliderect
-            if self.rect.colliderect(b) and self.velocity_yaxis >= 0: # checks if the player is colliding with a platform and is falling downwards 
-                self.rect.bottom = b.top
-                self.velocity_yaxis = 0 # resets y velocity so u dont keep falling through the platform
-                self.grounded = True # sets grounded to true because the player is on a platform
-                self.jcount = 0 # jumps set back to 0 because u landed
+    def draw(self, xcam): # just draws the cube
+        pygame.draw.rect(screen, White, (self.rect.x - xcam, self.rect.y, self.rect.width, self.rect.height))
 
-            if self.rect.bottom >= groundY: # ground collision stuff
-                self.rect.bottom = groundY
-                self.velocity_yaxis = 0
-                self.grounded = True
-                self.jcount = 0
-
-# testing features and stuff here
-    def draw(self, surface, pos_x):
-       # info on rect: https://www.pygame.org/docs/ref/rect.html
-       pygame.draw.rect(surface, (255, 255, 255), # js draws a white rectangle, its pretty obvious but ill comment it anyway because its my code :imp:
-                        (self.rect.x - 
-                        pos_x, 
-                        self.rect.y, 
-                        self.rect.w, 
-                        self.rect.h))
-
-player = Player(100, 600)
-
-# score board kinda 
-
-camera = 0
-score = 0
-farthest_plat = (c.x for c in platforms) # idk if ima use this but im making it in the hope its useful later,
-                                         # because i might need to find the farthest platform for something or if i add inf generation i might need this to stop inf yield or something
-
-def new_plat():
-    global farthest_plat # cant remember if we discussed global but incase, info is from https://www.w3schools.com/python/python_variables_global.asp
-    last = platforms - 1  # removes the last platforms
-
+# objects
+player = cube(200, 500) # cube dimensions
+platforms = [pygame.Rect(150, 550, 220, 20)] # dimensions of the generating platforms
+score = 0 # score tracker
+xcam = 0 # basically just how far right to move the camera when follwoing the cube
+dead = 720 + 50 # just means if u go too low it breaks and resets u
 
 # game loop
 playing = True
-while playing: # simple loop to keep the window running
-    for x in pygame.event.get():
-        if x.type == pygame.QUIT:
+
+while playing:
+    clock.tick(60) # fps
+    screen.fill(Background) # gives the game a background obv 
+    
+    for event in pygame.event.get():    # we did this in class
+        if event.type == pygame.QUIT:   # and it was also in the mario game example
             playing = False
+        
+        if state == "menu" and event.type == pygame.MOUSEBUTTONDOWN: # lets me make a play button that works when you click it
+            if start.collidepoint(event.pos):                        # lots of info was found on google, using llms and this specific
+                state = "gaming"                                     # pygame doc: https://www.pygame.org/docs/ref/mouse.html
 
-    keys = pygame.key.get_pressed()
-    player.movement(keys, platforms)
+    if state == "menu": 
+        pygame.draw.rect(screen, Green, start) # draws the green rectangle that will have our play button in it
+        text = font1.render("play", True, White)
+        screen.blit(text, text.get_rect(center=start.center)) # google helped me figure out how to center it
+    else:
+        keyboard = pygame.key.get_pressed()
+        player.movement(platforms, keyboard)
+        xcam = player.rect.x - 200 # so the camera is slightly offset so it easier to follow the player instead of being in the exact middle
+                                   # also an example would be if the player is at 500, the cam would be at 300 because the xcam runs this 500-200=x 
+                                   # to figure out where to position itself and it does this until reset or the games is closed
 
-# background, contant screen update
-    screen.fill((30, 30, 40)) # dark blueish almost black background
-    for a in platforms:
-        pygame.draw.rect(screen, (100, 200, 100), a) # green rectangles for platforms rn bc im lazy 1 and 2 im not done building yet :rage:
-    player.draw(screen)
-    pygame.display.flip() 
-    clock.tick(60)
+### LLM used to fix
+        # Generate platforms ahead
+        farthest_x = max(p.right for p in platforms)
+        while farthest_x < player.rect.x + 1280:
+            gap_x = random.randint(120, 180)
+            gap_y = random.randint(-50, 50)
+            prev_y = platforms[-1].y
+            new_y = max(100, min(720 - 50, prev_y + gap_y))
+            new_platform = pygame.Rect(farthest_x + gap_x, new_y, 200, 20)
+            platforms.append(new_platform)
+            farthest_x = new_platform.right
+
+        # Remove old platforms behind player
+        platforms = [p for p in platforms if p.right > player.rect.x - 200]
+
+        # Draw platforms
+        for p in platforms:
+            pygame.draw.rect(screen, Green, (p.x - xcam, p.y, p.width, p.height))
+### end
+## explaination
+# same as last time where i couldnt find a perfect way to make the platforms generate as i went ahead,
+# used an LLM to make the code and save me the time from doing endless research as id already poured 2 hours on this one section
+# i also couldnt figure out what components to use fully and where, and stated earlier 
+# id rather have a game that works and i like rather than one that runs off hopes and dreams
+
+        player.draw(xcam) # drawing the camera for the player
+
+       
+        score = max(score, player.rect.x) # keeps score 
+        score_text = font1.render(f"Score: {score}", True, White) # font and color from: https://www.pygame.org/docs/ref/font.html
+        screen.blit(score_text, (10, 10)) # quick google search helped me figure out how blit works as i didnt fully understand from: https://www.pygame.org/docs/ref/surface.html
+
+        if player.rect.top > dead: # this basically just resets u instead of killing the window, it also made it easier to test, 
+            player.rect.x = 200    # more complex, it kills everything, generates the cube again, and puts the player and camera back at the start,
+            player.rect.y = 500    # this also resets the score
+            player.y_velocity = 0
+            player.onground = False
+            platforms = [pygame.Rect(150, 550, 220, 20)]
+            score = 0
+            xcam = 0
+
+    pygame.display.flip() # keeps the display goin
 
 pygame.quit()
-sys.exit()
-
-### more info on sys because i feel i wasnt clear enough to mac users lol, uhh basically theres these things called 
-### memory leaks which programs like discord and often chat messengers and game development softwares like unity and roblox studio are notorious for constantly using 
-### memory until it spikes to 100% and crashing the program or your pc, knowing how windows 11 is poorly coded in some aspects, without sys.exit sometimes ill close a pygame window 
-### but windows wont actually close it until you end task in task manager  
+sys.exit() # ive used no sys in the past and had memory leaks, obviously this isnt using a lot of ram but i still like to have it there from,
+           # half from habit and also it helps with other things
